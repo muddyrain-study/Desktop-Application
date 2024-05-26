7991b6527e87b6f56ab7bf1b452a4403be44713b
muddyrain <qiu3291002845@gmail.com>
2024-05-26 22:40:58 +0800

    feat: 多窗口管理

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const { app, BrowserWindow, ipcMain, MessageChannelMain } = require("electron");
 const url = require("url");
 const path = require("path");
-
+const winMap = new Map();
 const createWindow = (url, options) => {
   const win = new BrowserWindow({
-    width: 350,
-    height: 350,
-    transparent: true,
-    resizable: false,
-    frame: false,
+    width: 1200,
+    height: 700,
     webPreferences: {
       nodeIntegration: true, // 开启node集成
       contextIsolation: false, // 关闭上下文隔离
       webviewTag: true, // 允许使用 <webview> 标签
     },
+    ...options,
   });
   win.loadURL(url);
-  win.setAlwaysOnTop(true, "pop-up-menu");
   // 设置窗口忽略鼠标事件
   // win.setIgnoreMouseEvents(true);
+  winMap.set(options.id, win);
+  if (options.id === "win2") {
+    win.on("close", (e) => {
+      e.preventDefault();
+      win.hide();
+    });
+  }
   return win;
 };
-
-ipcMain.on("setIgnoreMouseEvent", (event, ...arg) => {
-  const win = BrowserWindow.fromWebContents(event.sender);
-  BrowserWindow.fromWebContents(event.sender).setIgnoreMouseEvents(...arg);
-});
 
 app.whenReady().then(() => {
   const url1 = url.format({
@@ -33,5 +32,35 @@ app.whenReady().then(() => {
     slashes: true,
     pathname: path.join(__dirname, "window", "" + "index.html"),
   });
-  createWindow(url1);
+  const url2 = url.format({
+    protocol: "file",
+    slashes: true,
+    pathname: path.join(__dirname, "window2", "" + "index.html"),
+  });
+  createWindow(url1, {
+    id: "win1",
+    show: true,
+  });
+  createWindow(url2, {
+    id: "win2",
+    show: false,
+    width: 800,
+    height: 600,
+  });
+});
+
+ipcMain.on("openWindow", (event, arg) => {
+  if (arg === "win1") {
+    winMap.get("win1").show();
+  } else if (arg === "win2") {
+    winMap.get("win2").show();
+  }
+});
+
+ipcMain.on("closeWindow", (event, arg) => {
+  if (arg === "win1") {
+    winMap.get("win1").hide();
+  } else if (arg === "win2") {
+    winMap.get("win2").hide();
+  }
 });
--- window/index.js
+++ window/index.js
@@ -1,54 +1,12 @@
 const { ipcRenderer } = require("electron");
 
-const deg = 6; // 360/60
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
 
-const hour = document.querySelector(".hour");
-const min = document.querySelector(".min");
-const sec = document.querySelector(".sec");
-
-setInterval(() => {
-  let day = new Date();
-  let hh = day.getHours() * 30;
-  let mm = day.getMinutes() * deg;
-  let ss = day.getSeconds() * deg;
-
-  hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
-  min.style.transform = `rotateZ(${mm}deg)`;
-  sec.style.transform = `rotateZ(${ss}deg)`;
-}, 1000);
-
-let offset = null;
-// 解决时钟拖拽移动问题
-document.addEventListener("mousedown", (e) => {
-  if (e.target.matches(".clock") || e.target.matches(".clock *")) {
-    window.isDragging = true;
-    offset = {
-      x: e.screenX - window.screenX,
-      y: e.screenY - window.screenY,
-    };
-  }
+// 这里我们想要在 win1 中控制 win2 的显示和隐藏
+openBtn.addEventListener("click", () => {
+  ipcRenderer.send("openWindow", "win2");
 });
-
-document.addEventListener("mousemove", (e) => {
-  if (window.isDragging) {
-    window.screenX = e.screenX - offset.x;
-    window.screenY = e.screenY - offset.y;
-    window.moveTo(window.screenX, window.screenY);
-  }
-});
-
-document.addEventListener("mouseup", () => {
-  window.isDragging = false;
-  offset = null;
-});
-
-const clock = document.querySelector(".clock");
-
-clock.addEventListener("mouseEnter", (e) => {
-  console.log("mouseEnter");
-  ipcRenderer.send("setIgnoreMouseEvent", false);
-});
-
-clock.addEventListener("mouseLeave", (e) => {
-  ipcRenderer.send("setIgnoreMouseEvent", true, { forward: true });
+closeBtn.addEventListener("click", () => {
+  ipcRenderer.send("closeWindow", "win2");
 });
